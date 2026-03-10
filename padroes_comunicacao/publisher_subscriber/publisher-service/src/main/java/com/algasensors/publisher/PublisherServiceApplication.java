e512e35fcbde4ff6701e7dedfa5191f65717f5a6
Vinícius Henrique Dias de Souza <vinihds@gmail.com>
2026-03-10 11:38:42 -0300

    Implementado padrao de comunicacao Publisher/Subscriber (Um publica para um tópico; vários podem se inscrever e receber)

---
+++ padroes_comunicacao/publisher_subscriber/publisher-service/src/main/java/com/algasensors/publisher/PublisherServiceApplication.java
@@ -0,0 +1,11 @@
+package com.algasensors.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PublisherServiceApplication.class, args);
+    }
+}
